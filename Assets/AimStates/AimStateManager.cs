394e8f4574e52e7e4d7fd9adfd59267acb51f154
edaagunes <edagunes00@hotmail.com>
2024-02-20 22:48:12 +0300

    Weapon Aiming

--- Assets/AimStates/AimStateManager.cs
+++ Assets/AimStates/AimStateManager.cs
@@ -21,11 +21,15 @@ public class AimStateManager : MonoBehaviour
     [HideInInspector] public float hipFov;
     [HideInInspector] public float currentFov;
     public float fovSmoothSpeed = 10;
+
+    [SerializeField]  Transform aimPos;
+    [SerializeField]  float aimSmoothSpeed = 20;
+    [SerializeField]  LayerMask aimMask;
      void Start()
      {
          vCam = GetComponentInChildren<CinemachineVirtualCamera>();
          hipFov = vCam.m_Lens.FieldOfView;
-         anim = GetComponentInChildren<Animator>();
+         anim = GetComponent<Animator>();
         SwitchState(Hip);
     }
 
@@ -36,7 +40,12 @@ public class AimStateManager : MonoBehaviour
         yAxis = Mathf.Clamp(yAxis, -80, 80);
 
         vCam.m_Lens.FieldOfView = Mathf.Lerp(vCam.m_Lens.FieldOfView, currentFov, fovSmoothSpeed * Time.deltaTime);
-        
+
+        Vector2 screenCentre = new Vector2(Screen.width / 2, Screen.height / 2);
+        Ray ray = Camera.main.ScreenPointToRay(screenCentre);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, aimMask))
+            aimPos.position = Vector3.Lerp(aimPos.position, hit.point, aimSmoothSpeed * Time.deltaTime);
         currentState.UpdateState(this);
     }
 
